90d436f39ec778234fedd1edce047397bd24c490
HoangDavid <hoangnguyenxuan2004@gmail.com>
2024-06-28 02:37:22 +0700

    [feat] add hard-coded bot

--- code/src/Play.tsx
+++ code/src/Play.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback } from "react"
+import React, {useState, useEffect} from "react"
 import WordManger from "./components/WordManager"
 import styled from '@mui/styled-engine'
 import { TextField, Box, Typography } from "@mui/material"
@@ -8,6 +8,20 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import './play.css'
+import bot from './assets/bot.gif'
+import CursorSVG from './assets/cursor.svg'
+
+const instructions= [
+    'Chào mừng bạn đến AI là Vua Tiếng Việt!',
+    'Trước khi chơi thì mình sẽ giải thích luật chơi nhé!',
+    'Mình sẽ đưa ra một từ tiếng Việt, bạn cần phải nối từ tiếp theo mà bạn nghĩ đến.',
+    'Để thắng cuộc, bạn cần phải trả lời đúng 15 câu hỏi.',
+    'Không được trả lời trễ quá thời gian, nếu không bạn sẽ thua cuộc.',
+    'Chúc bạn may mắn!',
+]
+let instruction_index = 0
+
 
 function Play(){
     // Styling
@@ -23,22 +37,13 @@ function Play(){
         margin: '0 auto'
     }
 
-    const Title = styled(Typography)({
-        position: 'relative',
-        textAlign: 'center',
-        fontWeight: 800,
-        fontSize: 25,
-        display: 'block',
-        textShadow: '0 0 10px #5A4EAE, 0 0 20px #5A4EAE, 0 0 30px #5A4EAE, 0 0 40px #5A4EAE, 0 0 50px #5A4EAE, 0 0 60px #5A4EAE, 0 0 70px #5A4EAE',
-    })
-
     const Timer = styled('div')({
         position: 'relative',
         textAlign: 'center',
         fontWeight: 800,
         fontSize: 40,
         display: 'block',
-        marginTop: '5%',
+        marginTop: '2%',
     })
 
 
@@ -49,7 +54,7 @@ function Play(){
         width: 'fit-content',
         verticalAlign: 'middle',
         height: '70px',
-        marginTop: '15%'
+        marginTop: '5%'
     }
 
     const Prefix = styled('div')({
@@ -101,126 +106,186 @@ function Play(){
         }
     }
 
-    // Game Logic
-    const [CURRENT_STATE, SET_CURRENT_STATE] = useState<string>('lose')
+    // GAME LOGIC
+    const [CURRENT_STATE, SET_CURRENT_STATE] = useState<string>('start')
     const words = WordManger()
     const [prefix, setPrefix] = useState<string>('')
     const [tagAnswer, setTagAnswer] = useState<string[]>([])
     const [isCorrect, setIsCorrect] = useState<boolean>(false)
+    const [enterPressed, setEnterPressed] = useState(false);
     const [correctAsnwer, setCorrectAnswer] = useState<number>(0)
-    
+
+    const [BotPrefix, setBotPrefix] = useState<string>('')
 
     // Timer and time constraints
     const [timer, setTimer] = useState(15)
     const [timeConstraint, setTimeConstraint] = useState(14)
 
-    // Player answer
+    const getRandomWord = () => {
+        const keys = Object.keys(words).filter(key => words[key].length > 10)
+        const k = keys[Math.floor(Math.random() * keys.length)]
+        const v = words[k] as string[]
+        setBotPrefix(k)
+
+        let tmp = 0
+        let key = ''
+        for (let i = 0; i < v.length; i++){
+            if (v[i] in words && words[v[i]].length > tmp){
+                tmp = words[v[i]].length
+                key = v[i]
+            }
+        }
+        
+        const value = words[key] as string[]
+        
+        return [key, value]
+    }
+    // Initialize a word
+    useEffect(() => {
+        if (CURRENT_STATE == 'play' && Object.keys(words).length > 0 && prefix === '') {
+            const [key, value] = getRandomWord()
+            setPrefix(key as string)
+            setTagAnswer(value as string[])
+        }
+    }, [words, CURRENT_STATE])
+
+    // Get player answer
     const [answer, setAnswer] = useState('')
     const checkAnswer = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === 'Enter'){
-            setAnswer('')
             if (tagAnswer.includes(answer)){
                 setIsCorrect(true)
+                setEnterPressed(prev => !prev)
+                setAnswer('')
             }else{
                 setAnswer('')
             }
         }
     }
-
-    //Restart timer and randomize the word
-    const restart = useCallback(() => {
-        setTimer(15)
-        setTimeConstraint(14)
-        setAnswer('')
-    }, [])
-
-    // Randomize the word
-    const randomWord = useCallback(() => {
-        const keys = Object.keys(words).filter(key => words[key].length > 8)
-        const key = keys[Math.floor(Math.random() * keys.length)]
-        const value = words[key] as string[]
-        setPrefix(key)
-        setTagAnswer(value)
-    }, [words, setPrefix, setTagAnswer])
-
-
-    // Initialize a word
-    useEffect(() => {
-        if (Object.keys(words).length > 0 && prefix === '') {
-            randomWord()
-        }
-    }, [words, prefix, randomWord])
     
     // Reset timer if no correct guess and with time constraint if corrrect guess
     useEffect(() => {
-        // if (isCorrect){ // this part keep running twice
-        //     setTimer(timeConstraint)
-        //     if (timeConstraint > 5){
-        //         setTimeConstraint(timeConstraint - 1)
-        //     }
-        //     const tmp = prefix + answer
-        //     if (!correctAsnwer.includes(tmp)){
-        //         correctAsnwer.push(tmp)
-        //         console.log(correctAsnwer)
-        //     }
-        //     setIsCorrect(false)
-        //     randomWord()
-        // }
-        if (timer > 0) {
-            const id = setInterval(() => {
-                setTimer(timer - 1)
-            }, 1000)
+        if (CURRENT_STATE === 'play'){
+            if (timer > 0) {
+                const id = setInterval(() => {
+                    setTimer(timer - 1)
+                }, 1000)
+                return () => clearInterval(id)
 
-            return () => clearInterval(id)
-
-        } else{
-            setTimer(timeConstraint)
-            if (timeConstraint > 5){
-                setTimeConstraint(timeConstraint - 1)
+            } else{
+                setTimer(timeConstraint)
+                if (timeConstraint > 5){
+                    setTimeConstraint(timeConstraint - 1)
+                }
+                if (!isCorrect){
+                    setOpen(true)
+                    SET_CURRENT_STATE('lose')
+                    setTimer(15)
+                    setTimeConstraint(14)
+                }
+                setAnswer('')
             }
-            if (!isCorrect){
-                setOpen(true)
-                SET_CURRENT_STATE('lose')
-                restart()
-            }
-            randomWord()
-            setAnswer('')
         }
-    }, [timer, timeConstraint, isCorrect, correctAsnwer, prefix, answer, randomWord, restart])
 
+    }, [timer, CURRENT_STATE])
+
+    // Check if player answer is correct
     useEffect(() => {
-        if (isCorrect){ 
-            setTimer(timeConstraint)
-            if (timeConstraint > 5){
-                setTimeConstraint(timeConstraint - 1)
-            }
+        if (isCorrect){
             setCorrectAnswer(correctAsnwer + 1)
-            setIsCorrect(false)  
-            randomWord()
+            setIsCorrect(false)
+            
+            // Check if player wins
+            if (correctAsnwer == 14){
+                setOpen(true)
+                SET_CURRENT_STATE('win')
+                setTimer(15)
+                setTimeConstraint(14)
+            }else{
+                setTimer(timeConstraint)
+                if (timeConstraint > 5){
+                    setTimeConstraint(timeConstraint - 1)
+                }
+                setAnswer('')
+                const [key, value] = getRandomWord()
+                setPrefix(key as string)
+                setTagAnswer(value as string[])
+            }
         }
-    }, [isCorrect, timeConstraint, correctAsnwer, randomWord])
-
-    useEffect(() => {
-        if (correctAsnwer == 15){
-            setOpen(true)
-            SET_CURRENT_STATE('win')
-        }
+    }, [enterPressed])
     
-    }, [correctAsnwer])
-
-    
-    // Dialog Popup
+    // Dialog Popup, stop timer and restart game
     const [open, setOpen] = useState(false)
-    const toggleDialog = () => (setOpen(!open))
+    const toggleDialog = () => {
+        setOpen(!open)
+        SET_CURRENT_STATE('start')
+        setCorrectAnswer(0)
+        instruction_index = 0
+    }
+
+    // Bot typing effect
+    const [displayResponse, setDisplayResponse] = useState('');
+    const [completedTyping, setCompletedTyping] = useState(false);
+    
+    useEffect(() => {
+        setCompletedTyping(false);
+        let stringResponse = '';
+        if (CURRENT_STATE == 'start'){
+            stringResponse = instructions[instruction_index]
+        }else if (CURRENT_STATE == 'play'){
+            stringResponse = `Từ tiếp theo là: ${BotPrefix} ${prefix}`
+        }
+        let i = 0
+        const intervalId = setInterval(() => {
+          setDisplayResponse(stringResponse.slice(0, i));
+      
+          i++;
+      
+          if (i > stringResponse.length) {
+            clearInterval(intervalId);
+            setCompletedTyping(true);
+            
+            if (CURRENT_STATE == 'start' && instruction_index < instructions.length - 1) {
+                setTimeout(() => {
+                    instruction_index++;
+                    setDisplayResponse('');
+                    setCompletedTyping(false);
+                }, 4000)
+            }else if (CURRENT_STATE == 'start' && instruction_index == instructions.length - 1){
+                setTimeout(() => {
+                    SET_CURRENT_STATE('play')
+                }, 4000)
+            }
+          }
+        }, 20);
+
+      
+        return () => clearInterval(intervalId);
+      }, [instructions, instruction_index, prefix, BotPrefix, CURRENT_STATE]);
 
     return (
     <>
         <Box sx={main}>
             <Box sx={container}>
-                <Title sx={{marginTop: '2%'}}>TECHDAY 2024</Title>
-                <Title> NỐI TỪ</Title>
+                <div className="title" style={{marginTop: '2%'}}>TECHDAY 2024</div>
+                <div className="title">NỐI TỪ</div>
                 <Timer>{timer}</Timer>
                 Correct: {correctAsnwer} / 15
+                <div style={{display: 'block',marginLeft: '10%'}}>
+                    <img src={bot} alt="" style={{verticalAlign: 'middle'}}/>
+                    <div className="talk-bubble tri-right left-in">
+                        <span>
+                        {displayResponse}
+                        { !completedTyping && <svg
+                        viewBox="8 4 8 16"
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="cursor"
+                        >
+                        <rect x="10" y="6" width="4" height="12" fill="#fff" />
+                        </svg>}
+                        </span>
+                    </div>
+                </div>
                 <Box sx={playzone}>
                     <Prefix>
                         <Typography sx={{color: 'black', 
